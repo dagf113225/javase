19334ff316ea705b4f796269aed67ecba223bc06
dagf113225 <512444862@qq.com>
2018-11-04 22:08:44 +0800

    TestTwo代码维护

--- javaone/src/TestTwo.java
+++ javaone/src/TestTwo.java
@@ -17,6 +17,43 @@ public class TestTwo {
 		System.out.println(a2);
 		
 		
+		//3.整型
+		int  a3=300;
+		System.out.println(a3);
+		
+		
+		//4.长整型
+		long  a4=6000;
+		System.out.println(a4);
+		
+		System.out.println("----------------------");
+		
+		
+		//5.boolean类型
+		boolean  flag1 =true;
+		
+		System.out.println(flag1);
+		
+		boolean  flag2 =false;
+		System.out.println(flag2);
+		
+		System.out.println("----------------------");
+		
+		//6.单精度浮点类型,
+		float   f1=20.0f;
+		System.out.println(f1);
+		
+		//7.双进度 浮点类型。默认是double 类型
+		double d1 =20.0;
+		System.out.println(d1);
+		
+		System.out.println("----------------------");
+		
+		
+		//8.字符类型
+		char  cf='a';
+		System.out.println(cf);
+		
 		
 		
 		
