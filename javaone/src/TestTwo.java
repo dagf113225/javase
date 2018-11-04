ab693f7af89741a78026692949dd1da4cc08a6d4
dagf113225 <512444862@qq.com>
2018-11-04 21:36:16 +0800

    java添加案例二

---
+++ javaone/src/TestTwo.java
@@ -0,0 +1,29 @@
+
+public class TestTwo {
+	
+	public static void main(String[] args) {
+		
+		//java的基本数据类型
+		
+		//1.字节类型
+		byte  a1=100;
+		
+		System.out.println(a1);
+		
+		
+		//2.短整型
+		short  a2=200;
+		
+		System.out.println(a2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
