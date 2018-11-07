7d6e9f3c5222513f2f32d3eeea713ee6f9a8f5d9
dagf113225 <512444862@qq.com>
2018-11-07 10:44:56 +0800

    提交java数据类型和变量

---
+++ javaone/src/Students.java
@@ -0,0 +1,79 @@
+//一个学生类  自定义类型
+public class Students
+{
+	
+	//变量定义的时候，明确的是什么?1.变量类型 ;2.变量名称
+	//隐式的表达了，作用域:全局，局部。
+	 
+	//姓名""  String  class类
+	//只要首字母是大写的，肯定是类，class
+	//Sun公司的程序员
+
+	
+	//是否是团员
+	boolean  flag;
+	
+    //年龄  java整数类型 ：byte(字节),short(短整型),int(整型),long(长整型)
+	byte     age;
+	
+
+	
+	//学生编号
+	short   stunumber;  
+	
+	
+
+	//int 
+	
+	
+	//学生视频文件大小
+	long   fileSize;
+	
+	//身高  浮点类型  整数+小数  float单精度类型  32
+	//  double  双精度   64
+	float  stuHeight=120.9f;
+	
+	//浮点类型默认是 double，如果定义的是float ,后面必须加f;
+	float  a1=20.0f;
+	
+	//学生的账户的钱
+	double  stuMoney=20.005;
+	
+	//字符类型 
+	char  syusex='男';
+	
+	
+	//变量定义的时候，明确的是什么?1.变量类型 ;2.变量名称
+	//隐式的表达了，作用域:全局，局部。
+	 
+	//姓名""  String  class类
+	//只要首字母是大写的，肯定是类，class
+	//Sun公司的程序员
+	//字符串类型
+	String name="李欣"; 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		int  a;
+		
+		
+		
+	}
+	
+
+	 
+	
+	
+
+}
---
+++ javaone/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test
+{
+	
+	public static void main(String[] args)
+	{
+		String  s1 = new String("123");
+		String  s2 = new String("123");
+		
+		System.out.println(s1==s2);
+		
+		System.out.println("---------------");
+		
+		String  s3="123";
+		String  s4="123";
+		System.out.println(s3==s4);
+		
+	}
+
+}
